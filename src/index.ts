7b0ed089a496c48cbde723bd300ad2681c830796
arteemm <arteemmarteemm@gmail.com>
2024-04-12 22:21:48 +0300

    add link to router

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Express }  from 'express';
 import bodyParser from 'body-parser';
 import { blogsRouter } from './routes/blogs-router';
 import { postsRouter } from './routes/posts-router';
+import { testingRouter } from './routes/testing-router';
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ app.use(parserMiddleware);
 
 app.use('/blogs', blogsRouter);
 app.use('/posts', postsRouter);
+app.use('/testing', testingRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
