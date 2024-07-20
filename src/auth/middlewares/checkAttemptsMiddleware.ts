9545fa7b822c004a1dbaba7a42970fc97e1cb475
arteemm <arteemmarteemm@gmail.com>
2024-07-20 17:25:32 +0300

    fix 429 error

--- src/auth/middlewares/checkAttemptsMiddleware.ts
+++ src/auth/middlewares/checkAttemptsMiddleware.ts
@@ -10,32 +10,29 @@ const attempts: {[keyof: string] : {
 export const checkAttemptsMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const result = validationResult(req);
     const ip = (req.headers['x-forwarded-for'] || req.socket.remoteAddress) as string;
+    const url = `${ip}`+`${req.originalUrl}`;
 
     if (!result.isEmpty()) {
-            if (!attempts[ip+req.originalUrl]?.countAttempts) {
-                attempts[ip+req.originalUrl] = {
-                    timeFirstAttempt: Date.now(),
-                    countAttempts: 1,
-                }
-            } else {
-                attempts[ip+req.originalUrl].countAttempts++
+        if (!attempts[url]?.countAttempts) {
+            attempts[url] = {
+                timeFirstAttempt: Date.now(),
+                countAttempts: 1,
             }
-            if (attempts[ip+req.originalUrl].countAttempts > 5 && (Date.now() - attempts[ip+req.originalUrl].timeFirstAttempt) < 10000) {
-                return res.send(HTTP_STATUS_CODES.RATE_LIMITING)
-            }
-
-        if (attempts[ip+req.originalUrl].countAttempts > 5 && (Date.now() - attempts[ip+req.originalUrl].timeFirstAttempt) < 10000) {
-            return res.send(HTTP_STATUS_CODES.RATE_LIMITING)
+        } else {
+            attempts[url].countAttempts++
         }
 
+        if (attempts[url].countAttempts > 5 && (Date.now() - attempts[url].timeFirstAttempt) < 10000) {
+            return res.send(HTTP_STATUS_CODES.RATE_LIMITING)
+        }
         
         return next();
     }
 
-    if (attempts[ip+req.originalUrl].countAttempts > 5 && (Date.now() - attempts[ip+req.originalUrl].timeFirstAttempt) < 10000) {
+    if (attempts[url]?.countAttempts > 5 && (Date.now() - attempts[url]?.timeFirstAttempt) < 10000) {
         return res.send(HTTP_STATUS_CODES.RATE_LIMITING)
     }
-    attempts[ip+req.originalUrl] ? attempts[ip+req.originalUrl].countAttempts = 0 : attempts[ip+req.originalUrl];
+    attempts[url] ? attempts[url].countAttempts = 0 : attempts[url];
 
     return next();
 };
