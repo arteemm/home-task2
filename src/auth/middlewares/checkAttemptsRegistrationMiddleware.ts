13b469ebd2a8d6cc094a68824d2d38f149c7cdb6
arteemm <arteemmarteemm@gmail.com>
2024-07-20 18:01:23 +0300

    fix 429 error

---
+++ src/auth/middlewares/checkAttemptsRegistrationMiddleware.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Response, Request } from 'express';
+import { HTTP_STATUS_CODES } from '../../constants/httpStatusCodes';
+
+const attempts: {[keyof: string] : {
+    countAttempts: number;
+    timeFirstAttempt: number;
+}} = {};
+
+export const checkAttemptsRegistrationMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    const ip = (req.headers['x-forwarded-for'] || req.socket.remoteAddress) as string;
+    const url = `${ip}`+`${req.originalUrl}`;
+
+    if ((Date.now() - attempts[url]?.timeFirstAttempt) > 10000) {
+        attempts[url] ? attempts[url].countAttempts = 0 : attempts[url];
+    }
+
+    if (!attempts[url]?.countAttempts) {
+        attempts[url] = {
+            timeFirstAttempt: Date.now(),
+            countAttempts: 1,
+        }
+    } else {
+        attempts[url].countAttempts++
+    }
+
+    if (attempts[url].countAttempts > 5 && (Date.now() - attempts[url]?.timeFirstAttempt) < 10000) {
+        return res.send(HTTP_STATUS_CODES.RATE_LIMITING)
+    }
+
+    return next();
+};
