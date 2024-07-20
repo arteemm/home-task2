8b699e3443ac8ae5190ef7f20af03546b5635630
arteemm <arteemmarteemm@gmail.com>
2024-07-20 17:35:07 +0300

    fix 429 error

--- src/auth/middlewares/checkAttemptsMiddleware.ts
+++ src/auth/middlewares/checkAttemptsMiddleware.ts
@@ -8,31 +8,34 @@ const attempts: {[keyof: string] : {
 }} = {};
 
 export const checkAttemptsMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    const result = validationResult(req);
+    // const result = validationResult(req);
     const ip = (req.headers['x-forwarded-for'] || req.socket.remoteAddress) as string;
     const url = `${ip}`+`${req.originalUrl}`;
 
-    if (!result.isEmpty()) {
-        if (!attempts[url]?.countAttempts) {
-            attempts[url] = {
-                timeFirstAttempt: Date.now(),
-                countAttempts: 1,
-            }
-        } else {
-            attempts[url].countAttempts++
-        }
-
-        if (attempts[url].countAttempts > 5 && (Date.now() - attempts[url].timeFirstAttempt) < 10000) {
-            return res.send(HTTP_STATUS_CODES.RATE_LIMITING)
-        }
-        
-        return next();
+    if ((Date.now() - attempts[url]?.timeFirstAttempt) > 10000) {
+        attempts[url] ? attempts[url].countAttempts = 0 : attempts[url];
     }
 
-    if (attempts[url]?.countAttempts > 5 && (Date.now() - attempts[url]?.timeFirstAttempt) < 10000) {
+    // if (!result.isEmpty()) {
+    if (!attempts[url]?.countAttempts) {
+        attempts[url] = {
+            timeFirstAttempt: Date.now(),
+            countAttempts: 1,
+        }
+    } else {
+        attempts[url].countAttempts++
+    }
+
+    if (attempts[url].countAttempts > 5 && (Date.now() - attempts[url]?.timeFirstAttempt) < 10000) {
         return res.send(HTTP_STATUS_CODES.RATE_LIMITING)
     }
-    attempts[url] ? attempts[url].countAttempts = 0 : attempts[url];
+    
+        // return next();
+    // }
+
+    // if (attempts[url]?.countAttempts > 5 && (Date.now() - attempts[url]?.timeFirstAttempt) < 10000) {
+    //     return res.send(HTTP_STATUS_CODES.RATE_LIMITING)
+    // }
 
     return next();
 };
