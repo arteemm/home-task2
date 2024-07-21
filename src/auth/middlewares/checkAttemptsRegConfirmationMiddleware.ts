615bde1b0114efd52fdbd7e724cda2e0b9a0c7b3
arteemm <arteemmarteemm@gmail.com>
2024-07-21 10:51:46 +0300

    refactor middleware

--- src/auth/middlewares/checkAttemptsRegConfirmationMiddleware.ts
+++ src/auth/middlewares/checkAttemptsRegConfirmationMiddleware.ts
@@ -1,29 +1,25 @@
 import { NextFunction, Response, Request } from 'express';
 import { HTTP_STATUS_CODES } from '../../constants/httpStatusCodes';
 
-const attempts: {[keyof: string] : {
-    countAttempts: number;
-    timeFirstAttempt: number;
-}} = {};
+let attempts: {
+    url: string;
+    expirationDate: number;
+}[] = [];
 
 export const checkAttemptsRegConfirmationMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const ip = (req.headers['x-forwarded-for'] || req.socket.remoteAddress) as string;
     const url = `${ip}`+`${req.originalUrl}`;
 
-    if ((Date.now() - attempts[url]?.timeFirstAttempt) > 12000) {
-        attempts[url] ? attempts[url].countAttempts = 0 : attempts[url];
-    }
+    attempts = attempts.filter((item)=> (item.expirationDate - Date.now()) > 0);
+    attempts.push({
+        url,
+        expirationDate: Date.now() + 10000,
+    });
 
-    if (!attempts[url]?.countAttempts) {
-        attempts[url] = {
-            timeFirstAttempt: Date.now(),
-            countAttempts: 1,
-        }
-    } else {
-        attempts[url].countAttempts++
-    }
+    const result = attempts.filter(item => item.url === url);
 
-    if (attempts[url].countAttempts > 5 && (Date.now() - attempts[url]?.timeFirstAttempt) < 10000) {
+
+    if (result.length > 5) {
         return res.send(HTTP_STATUS_CODES.RATE_LIMITING)
     }
 
