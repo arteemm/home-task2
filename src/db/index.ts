21b2263352e62d2016bdd23e4d1e642a6d7685b5
arteemm <arteemmarteemm@gmail.com>
2024-12-19 19:56:15 +0300

    fix url

--- src/db/index.ts
+++ src/db/index.ts
@@ -10,7 +10,7 @@ const dbName = 'home-task';
 
 export async function runDb () {
     try {
-        await mongoose.connect(mongoURI + '/' + dbName)
+        await mongoose.connect(mongoURI)
         console.log('Connected successfully to mongo server');
     } catch {
         console.log('Can\'t connect to db');
